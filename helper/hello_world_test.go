526df3a9d9a64b9d920facb1e9ecd02cd152dd51
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-04 01:15:09 +0800

    test return error

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,27 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
+	result := HelloWorld("Ryans")
+
+	if result != "Hello, Ryan" {
+		// t.Fail()
+		// t.FailNow()
+		t.Error("Result is not Hello, Ryan")
+		// t.Fatal("Result is not Hello, Ryan")
+	}
+
+	fmt.Println("TestHelloWorld done")
+}
+
+func TestHelloWorldTrue(t *testing.T) {
 	result := HelloWorld("Ryan")
 
 	if result != "Hello, Ryan" {
-		panic("Result is not Hello, Ryan")
+		t.Fail()
 	}
 }
