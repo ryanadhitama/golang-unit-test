dab22a8ccde3fd50a745de258ab502a925fb92ad
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-04 14:16:46 +0800

    test from array struct

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -25,6 +25,37 @@ func TestSubtest(t *testing.T) {
 	})
 }
 
+func TestTable(t *testing.T) {
+	tests := []struct{
+		name string
+		request string
+		expected string
+	}{
+		{
+			name: "HelloWorld(Ryan)",
+			request: "Ryan",
+			expected: "Hello, Ryan",
+		},
+		{
+			name: "HelloWorld(Adit)",
+			request: "Adit",
+			expected: "Hello, Adit",
+		},
+		{
+			name: "HelloWorld(Putra)",
+			request: "Putra",
+			expected: "Hello, Putra",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			assert.Equal(t, test.expected, result)
+		})
+	}
+}
+
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Ryan")
 
