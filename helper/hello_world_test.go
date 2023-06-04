f8a731544fd8d81b71fb1baa20c9f927da1ad108
ryanadhitama <ryanadhitama2@gmail.com>
2023-06-04 13:42:54 +0800

    before and after test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -6,8 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMain(m *testing.M) {
+	fmt.Println("Before Test")
+
+	m.Run()
+
+	fmt.Println("After Test")
+}
+
 func TestHelloWorld(t *testing.T) {
-	result := HelloWorld("Ryans")
+	result := HelloWorld("Ryan")
 
 	if result != "Hello, Ryan" {
 		// t.Fail()
@@ -28,8 +36,7 @@ func TestHelloWorldTrue(t *testing.T) {
 }
 
 func TestHelloWorldAssert(t *testing.T) {
+	// assert refer to Fail, require refer to FailNow
 	result := HelloWorld("Adit")
 	assert.Equal(t, "Hello, Adit", result, "Result must be 'Hello, Adit'")
-
-	// assert refer to Fail, require refer to FailNow
 }
